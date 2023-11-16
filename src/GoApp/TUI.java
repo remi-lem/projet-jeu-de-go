4786ea52109d6b4dff5954c366c6d20492d59605
EstebanCRz <esteban.cardaillac@gmail.com>
2023-11-16 10:23:18 +0100

    Commande quit fonctionnelle :3

--- src/GoApp/TUI.java
+++ src/GoApp/TUI.java
@@ -1,7 +1,14 @@
 package GoApp;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class TUI {
     public static void main(String[] Args){
-
+        Scanner sc = new Scanner(System.in);
+        do {
+            // TODO : les autres commandes
+        }
+        while (!Objects.equals(sc.nextLine(), "quit"));
     }
 }
