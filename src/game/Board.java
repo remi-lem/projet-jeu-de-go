a31c7956d3bc06f9ddbd23e705c4bf596eb92896
TorielLink <115531902+TorielLink@users.noreply.github.com>
2024-01-04 15:07:10 +0100

    ToDo de la fonction play de Board fait

--- src/game/Board.java
+++ src/game/Board.java
@@ -191,8 +191,8 @@ public class Board {
                 if (verifyMyLiberties(neighbor[0], neighbor[1], color, visitedMap, false))
                    return true;
             if (isFirstCalled)
-                for (int i = 0; i < size; i++) for( int j = 0; j < size ; j ++)
-                    if(visitedMap[i][j]) removeStone(i, j, color);
+                for (int i = 0; i < size; i++) for (int j = 0; j < size; j++)
+                        if (visitedMap[i][j]) removeStone(i, j, color);
             return false;
         } else return true;
     }
