c6bf531d93e9069ed1cfc097673ffc8b59c57bb3
TorielLink <115531902+TorielLink@users.noreply.github.com>
2024-01-05 16:55:01 +0100

    README.md amélioration -> ajout du diagramme d'architecture

--- src/app/TUI.java
+++ src/app/TUI.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class TUI {
-    public static final int MIN_LENGHT_ARGS = 1;
-    public static final int MAX_LENGHT_ARGS = 3;
+    public static final int MIN_LENGTH_ARGS = 1;
+    public static final int MAX_LENGTH_ARGS = 3;
     public static void main(String[] args) throws RuntimeException, InterruptedException {
-        if (args.length < MIN_LENGHT_ARGS || args.length > MAX_LENGHT_ARGS)
+        if (args.length < MIN_LENGTH_ARGS || args.length > MAX_LENGTH_ARGS)
             System.err.println("Error: please run with good arguments");
         else {
             boolean errorArgs = false;
